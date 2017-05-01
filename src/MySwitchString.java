6ebcaded539196eb4cd64352b6794da4e24486d1
Thiago Bomfim <tbomfim@atlassian.com>
2017-05-01 14:17:22 +1000

    Initial commit

---
+++ src/MySwitchString.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Thiago Bomfim
+ * on 1/5/17.
+ */
+public class MySwitchString {
+    public static void main(String[] args) {
+        String name="Thiago Bomfim";
+        switch(name){
+            case "Ana":
+                System.out.println("Ana");
+                break;
+            case ("Roberto"):
+                System.out.println("Robertinho");
+                break;
+            case ("Thiago Bomfim"):
+                System.out.println("This is the man!");
+                break;
+        }
+    }
+}
